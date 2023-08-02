1f76686d24b66a002f0aa2545eccab82c19025bc
Mitchel Nuñez <u20245202@utp.edu.pe>
2023-08-01 19:36:25 -0500

    :construction: correción de querys

--- code/EDAQuerys.sql
+++ code/EDAQuerys.sql
@@ -6,12 +6,12 @@ SELECT * FROM accidente
 SELECT * FROM vehiculo
 
 --Pregunta 1: ¿Cuántos accidentes han ocurrido en áreas urbanas versus áreas rurales?
-SELECT DISTINCT Area FROM accidente
-SELECT AREA, COUNT(AREA) AS TOTAL FROM  accidente GROUP BY Area
+SELECT DISTINCT area FROM accidente
+SELECT area, COUNT(area) AS 'total_accidente' FROM  accidente GROUP BY Area
 
 --Pregunta 2: ¿Qué día de la semana tiene el mayor número de accidentes?
 SELECT * FROM accidente
-SELECT TOP 3 Dia, COUNT(Dia) AS TOTAL FROM accidente GROUP BY Dia ORDER BY Dia
+SELECT TOP 3 dia, COUNT(Dia) AS 'total' FROM accidente GROUP BY Dia ORDER BY total DESC
 
 --Pregunta 3: ¿Cuál es la edad promedio de los vehículos involucrados en accidentes según su tipo?
 
@@ -21,7 +21,8 @@ SELECT * FROM accidente
 SELECT AVG(EdadVehiculo) PromedioEdad FROM vehiculo
 
 SELECT TipoVehiculo, COUNT(AccidenteIndex) AS TotalAccidente, AVG(EdadVehiculo) AS EdadPromVehiculo FROM vehiculo
-GROUP BY TipoVehiculo ORDER BY TotalAccidente DESC
+GROUP BY TipoVehiculo
+ORDER BY TotalAccidente DESC
 
 SELECT COUNT(AccidenteIndex) FROM vehiculo GROUP BY AccidenteIndex
 
@@ -40,15 +41,19 @@ FROM (
 SELECT COUNT(*) AS Nulos FROM vehiculo WHERE EdadVehiculo is null
 
 
-SELECT EdadGrupo, COUNT(EdadGrupo) AS EdadCount, AVG(EdadVehiculo) AS PromedioEdad FROM(
-SELECT EdadVehiculo,
-	CASE
-	WHEN EdadVehiculo BETWEEN 0 AND 5 THEN 'nuevo'
-	WHEN EdadVehiculo BETWEEN 6 and 10 THEN 'regular'
-	ELSE 'viejo'
-	END AS EdadGrupo
-FROM vehiculo WHERE EdadVehiculo is not null) AS SubQuery
-GROUP BY EdadGrupo
+SELECT EdadGrupo, COUNT(EdadGrupo) AS EdadCount, AVG(EdadVehiculo) AS PromedioEdad
+FROM (
+    SELECT EdadVehiculo,
+           CASE
+           WHEN EdadVehiculo BETWEEN 0 AND 5 THEN 'nuevo'
+           WHEN EdadVehiculo BETWEEN 6 AND 10 THEN 'regular'
+           ELSE 'viejo'
+           END AS EdadGrupo
+    FROM vehiculo
+    WHERE EdadVehiculo IS NOT NULL
+) AS SubQuery
+GROUP BY EdadGrupo;
+
 
 --Pregunta 5: ¿Existen condiciones climáticas específicas que contribuyan a accidentes fatales?
 
@@ -59,12 +64,16 @@ SELECT DISTINCT Gravedad FROM accidente
 SELECT Condiciones_climaticas , COUNT(Gravedad) 'Accidentes_Totales'
 FROM accidente WHERE Gravedad = 'Fatal' GROUP BY Condiciones_climaticas ORDER BY Accidentes_Totales DESC
 
---Pregunta 6: ¿Los accidentes suelen implicar impactos en el lado izquierdo de los vehículos?
+--Pregunta 6:  ¿Cuántos accidentes ocurrieron para cada tipo de dirección de volante?
 
 SELECT * FROM vehiculo
 
-SELECT ManoIzquierda, COUNT(ManoIzquierda) AS Accidentes_Totales FROM vehiculo
-WHERE ManoIzquierda is not null GROUP BY ManoIzquierda 
+SELECT ManoIzquierda, COUNT(ManoIzquierda) AS Accidentes_Totales
+FROM vehiculo
+GROUP BY ManoIzquierda
+ORDER BY 2 DESC
+
+
 
 
 --Pregunta 7: ¿Existe alguna relación entre los propósitos del viaje y la gravedad de los accidentes?
@@ -84,7 +93,7 @@ FROM vehiculo v JOIN accidente a ON v.AccidenteIndex = a.AccidenteIndex
 GROUP BY v.PropositoViaje
 ORDER BY TotalGravedad DESC
 
---Pregunta 8: Calcular la antigüedad promedio de los vehículos involucrados en accidentes, considerando Luz diurna y punto de impacto Frontal:
+--Pregunta 8: Calcular la antigüedad promedio de los vehículos involucrados en accidentes, considerando Luz diurna y punto de impacto Frontal.
 
 USE vhac
 
@@ -93,9 +102,10 @@ SELECT * FROM accidente
 
 SELECT a.Condiciones_de_luz, v.PuntoImpacto, CAST(AVG(v.EdadVehiculo) AS int) AS Promedio_Años
 FROM vehiculo v JOIN accidente a ON v.AccidenteIndex = a.AccidenteIndex
---WHERE a.Condiciones_de_luz = 'Luz'
+WHERE a.Condiciones_de_luz = 'Luz'
 GROUP BY a.Condiciones_de_luz, v.PuntoImpacto
-HAVING a.Condiciones_de_luz  = 'Luz' AND v.PuntoImpacto = 'Frontal'
+--HAVING a.Condiciones_de_luz  = 'Luz' AND v.PuntoImpacto = 'Frontal'
+
 
 --Pregunta 9: Calcular el Porcentaje de Gravedad
 SELECT * FROM accidente
@@ -118,10 +128,13 @@ FROM accidente GROUP BY Gravedad
 
 --Pregunta 10: Contar el número total de accidentes agrupados por la gravedad y las condiciones del camino
 SELECT Gravedad, Condiciones_del_camino, COUNT(Condiciones_del_camino) AS Total
-FROM accidente GROUP BY Gravedad, Condiciones_del_camino ORDER BY Total DESC
+FROM accidente
+GROUP BY Gravedad, Condiciones_del_camino
+ORDER BY Total DESC
 
 
 --Pregunta 10: Contar el número total de accidentes agrupados por propósito de viaje y gravedad de accidentes?
 SELECT v.PropositoViaje, a.Gravedad, COUNT(a.Gravedad) AS Total
 FROM vehiculo v JOIN accidente a ON v.AccidenteIndex = a.AccidenteIndex
-GROUP BY v.PropositoViaje, a.Gravedad ORDER BY 1 DESC;
+GROUP BY v.PropositoViaje, a.Gravedad
+ORDER BY 1 DESC;
